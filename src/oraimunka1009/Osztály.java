032b6aa6f683141684633d18f9356227c5f5782b
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-10-09 12:43:23 +0200

    first commmit

---
+++ src/oraimunka1009/Osztály.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oraimunka1009;
+
+/**
+ *
+ * @author somogyi.a.istvan
+ */
+public class Osztály {
+    
+}
