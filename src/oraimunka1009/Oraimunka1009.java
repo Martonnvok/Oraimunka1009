8f7f72f363f1087fc5c35eee2527a9ed15a1f9a4
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-10-09 13:51:30 +0200

    negyedik feladat

--- src/oraimunka1009/Oraimunka1009.java
+++ src/oraimunka1009/Oraimunka1009.java
@@ -9,8 +9,17 @@ public class Oraimunka1009 {
     private static void feladat() {
         Koord k1 = new Koord(20, 30);
         Negyzet n1 = new Negyzet(5, k1);
+        Negyzet n2 = new Negyzet(4, k1);
 
         System.out.println(n1.toString());
+        System.out.println(n2.toString());
+        
+        if(n1.getA() == n2.getA()){
+            System.out.println("Egyforma az oldaluk");
+        }
+        else{
+            System.out.println("Nem egyforma az oldaluk");
+        }
 
     }
 
